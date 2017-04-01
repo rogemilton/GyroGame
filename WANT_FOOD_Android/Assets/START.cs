455373134bd66b03299749b7afb508ab85c55a95
Louis <Level.100.Parasect@gmail.com>
2017-04-01 15:04:40 -0400

    Credits scene added

--- WANT_FOOD_Android/Assets/START.cs
+++ WANT_FOOD_Android/Assets/START.cs
@@ -2,9 +2,8 @@
 using System.Collections;
 
 public class START : MonoBehaviour {
-
-	public static string nameObject = "food";
     public Texture2D icon;
+    public Texture2D icon2;
 
 	void Start()
 	{
@@ -13,12 +12,14 @@ public class START : MonoBehaviour {
 
 	void OnGUI()
 	{
-			GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = GUI.skin.textField.fontSize = 70;
+        if(GUI.Button(new Rect(Screen.width*.1f, Screen.height*.6f, Screen.width * .8f,Screen.height*.3f), icon, GUIStyle.none))
+		{
+		    Application.LoadLevel(1);
+		}
 
-			if(GUI.Button(new Rect(Screen.width*.1f, Screen.height*.6f, Screen.width * .8f,Screen.height*.3f), icon, GUIStyle.none))
-			{
-					Application.LoadLevel(1);
-			}
-
-	}
+        if (GUI.Button(new Rect(Screen.width * .8f, Screen.height * .8f, Screen.width * .8f, Screen.height * .8f), icon2, GUIStyle.none))
+        {
+            Application.LoadLevel("credits");
+        }
+    }
 }
