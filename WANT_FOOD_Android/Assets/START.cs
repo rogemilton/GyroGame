e496b9009d130dcd2a1998317751bdc72c907c9c
Louis <Level.100.Parasect@gmail.com>
2017-04-11 19:59:24 -0400

    Added font file

--- WANT_FOOD_Android/Assets/START.cs
+++ WANT_FOOD_Android/Assets/START.cs
@@ -14,18 +14,12 @@ public class START : MonoBehaviour {
 	{
 			GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = GUI.skin.textField.fontSize = 70;
 
-			/*
-			if(GUI.Button(new Rect(Screen.width*.1f, Screen.height*.6f, Screen.width*.8f,Screen.height*.3f), "S T A R T"))
-			{
-					Application.LoadLevel(1);
-			}
-			*/
 			if(GUI.Button(new Rect(Screen.width*.35f, Screen.height*.4f, Screen.width*.4f, Screen.height*.4f), buttonPlay, GUIStyle.none))
 			{
 					Application.LoadLevel(1);
 			}
 
-			if (GUI.Button(new Rect(Screen.width*.35f, Screen.height*.8f, Screen.width*.4f, Screen.height*.4f), buttonCredits, GUIStyle.none))
+			if (GUI.Button(new Rect(Screen.width*.5f, Screen.height*.8f, Screen.width*.1f, Screen.height*.1f), buttonCredits, GUIStyle.none))
 			{
 					Application.LoadLevel("credits");
 			}
