c72d7b1dd84e07412698f95c08dd9f95e4d71117
Louis <Level.100.Parasect@gmail.com>
2017-04-16 17:13:31 -0400

    Adding buttons and new scenes

--- WANT_FOOD_Android/Assets/START.cs
+++ WANT_FOOD_Android/Assets/START.cs
@@ -4,26 +4,44 @@ using System.Collections;
 public class START : MonoBehaviour {
     public Texture2D buttonPlay;
     public Texture2D buttonCredits;
+    public Texture2D buttonSettings;
+    public Texture2D buttonHowTo;
+    public Texture2D buttonLeaderBoard;
 
-	void Start()
+    void Start()
 	{
 
 	}
 
 	void OnGUI()
 	{
-			GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = GUI.skin.textField.fontSize = 70;
+	    GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = GUI.skin.textField.fontSize = 70;
 
-			if(GUI.Button(new Rect(Screen.width*.35f, Screen.height*.4f, Screen.width*.4f, Screen.height*.4f), buttonPlay, GUIStyle.none))
-			{
-					Application.LoadLevel(1);
-			}
+	    if(GUI.Button(new Rect(Screen.width*.35f, Screen.height*.4f, Screen.width*.4f, Screen.height*.4f), buttonPlay, GUIStyle.none))
+	    {
+		    Application.LoadLevel(1);
+	    }
 
-			if (GUI.Button(new Rect(Screen.width*.5f, Screen.height*.8f, Screen.width*.1f, Screen.height*.1f), buttonCredits, GUIStyle.none))
-			{
-					Application.LoadLevel("credits");
-			}
+	    if (GUI.Button(new Rect(Screen.width*.45f, Screen.height*.7f, Screen.width*.1f, Screen.height*.1f), buttonLeaderBoard, GUIStyle.none))
+	    {
+		    //Application.LoadLevel("credits");
+	    }
 
-	}
+        if (GUI.Button(new Rect(Screen.width * .15f, Screen.height * .85f, Screen.width * .1f, Screen.height * .1f), buttonHowTo, GUIStyle.none))
+        {
+            //Application.LoadLevel("credits");
+        }
+
+        if (GUI.Button(new Rect(Screen.width * .45f, Screen.height * .85f, Screen.width * .1f, Screen.height * .1f), buttonSettings, GUIStyle.none))
+        {
+            //Application.LoadLevel("credits");
+        }
+
+        if (GUI.Button(new Rect(Screen.width * .75f, Screen.height * .85f, Screen.width * .1f, Screen.height * .1f), buttonCredits, GUIStyle.none))
+        {
+            Application.LoadLevel("credits");
+        }
+
+    }
 
 }
